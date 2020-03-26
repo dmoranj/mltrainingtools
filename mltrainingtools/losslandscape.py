20d451e5422cf899cee4f80472b01babe114c0ce
dmoranj <dmoranj@gmail.com>
2020-03-26 08:04:35 +0100

    Add GPU to the loss landscape tests

--- mltrainingtools/losslandscape.py
+++ mltrainingtools/losslandscape.py
@@ -35,7 +35,7 @@ class Net(nn.Module):
 
 
 def load_dataset():
-        # transforms
+    # transforms
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5,), (0.5,))])
@@ -51,19 +51,20 @@ def load_dataset():
                                                 transform=transform)
 
     # dataloaders
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=40,
                                               shuffle=True, num_workers=2)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=40,
                                              shuffle=False, num_workers=1)
 
     return trainloader, testloader
 
 
-def load_network():
+def load_network(device):
     net = Net()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    net = net.to(device)
 
     return net, criterion, optimizer
 
@@ -78,7 +79,7 @@ def select_directions(model):
 
     for parameter in model.named_parameters():
         direction = select_direction(parameter[1].shape)
-        normalized_direction = direction * parameter[1].norm().detach().numpy()
+        normalized_direction = direction * parameter[1].norm().cpu().detach().numpy()
 
         directions[parameter[0]] = normalized_direction
 
@@ -89,24 +90,26 @@ def perturb(base_parameters, north, south, i, j, step_size):
     new_values = []
 
     for name, parameter in base_parameters():
-        new_value = parameter.detach().numpy() + i*step_size*north[name] + j*step_size*south[name]
+        new_value = parameter.cpu().detach().numpy() + i*step_size*north[name] + j*step_size*south[name]
         assert(new_value.shape == parameter.shape)
         new_values.append((name, torch.tensor(new_value)))
 
     return OrderedDict(new_values)
 
 
-def estimate_loss(model, loss, dataset, new_parameters, max_batch=5):
+def estimate_loss(model, device, loss, dataset, new_parameters, max_batch=5):
     model.load_state_dict(new_parameters)
 
     accumulated_loss = []
 
     for i, data in enumerate(dataset, 0):
         inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
         batch_size = inputs.shape[0]
         predictions = model(inputs)
         loss_value = loss(predictions, labels)
-        accumulated_loss.append(loss_value.detach().numpy()/batch_size)
+        accumulated_loss.append(loss_value.cpu().detach().numpy()/batch_size)
 
         if i > max_batch:
             break
@@ -114,8 +117,9 @@ def estimate_loss(model, loss, dataset, new_parameters, max_batch=5):
     return sum(accumulated_loss)/len(accumulated_loss)
 
 
-def generate_loss_grid(model, loss, dataset, north, south, step_size=0.1, steps=10):
+def generate_loss_grid(model, device, loss, dataset, north, south, step_size=0.1, steps=10):
     log = section_logger(1)
+    sublog = section_logger(2)
 
     samples = np.zeros((steps, steps))
 
@@ -125,7 +129,7 @@ def generate_loss_grid(model, loss, dataset, north, south, step_size=0.1, steps=
         log('Computing row {}'.format(i))
         for j in range(steps):
             new_parameters = perturb(base_parameters, north, south, i, j, step_size)
-            loss_value = estimate_loss(model, loss, dataset, new_parameters)
+            loss_value = estimate_loss(model, device, loss, dataset, new_parameters)
             samples[i, j] = loss_value
 
     return samples
@@ -135,10 +139,13 @@ def plot_landscape(grid):
     pass
 
 
-def train(model, criterion, optimizer, ds_train):
+def train(model, device, criterion, optimizer, ds_train):
+    log = section_logger(1)
+
     running_loss = 0.0
 
     for epoch in range(1):  # loop over the dataset multiple times
+        log("Training epoch {}".format(epoch))
 
         for i, data in enumerate(ds_train, 0):
 
@@ -149,6 +156,8 @@ def train(model, criterion, optimizer, ds_train):
             optimizer.zero_grad()
 
             # forward + backward + optimize
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -162,23 +171,23 @@ def train(model, criterion, optimizer, ds_train):
 def execute():
     log = section_logger()
 
-    # TODO: add everything to CUDA
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     log("Load dataset")
     ds_train, ds_test = load_dataset()
 
     log("Load network")
-    model, loss, optimizer = load_network()
+    model, loss, optimizer = load_network(device)
 
     log("Train network")
-    model = train(model, loss, optimizer, ds_train)
+    model = train(model, device, loss, optimizer, ds_train)
 
     log("Sample two random directions")
     north = select_directions(model)
     south = select_directions(model)
 
     log("Generate grid of loss values")
-    grid = generate_loss_grid(model, loss, ds_test, north, south)
+    grid = generate_loss_grid(model, device, loss, ds_test, north, south)
 
     log("Plot landscape")
     plot_landscape(grid)
